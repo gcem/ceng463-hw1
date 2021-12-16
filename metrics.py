cb6f783fa4444c4e9e136634e3642064597f9dd2
gcem <34423323+gcem@users.noreply.github.com>
2021-12-16 19:55:58 +0300

    add metric code and change some refs

---
+++ metrics.py
@@ -0,0 +1,3 @@
+precision = true_positives / (true_positives + false_positives)
+recall = true_positives / (true_positives + false_negatives)
+f1_measure = 2 * precision * recall / (precision + recall)
